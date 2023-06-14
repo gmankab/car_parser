bf49606e0fe679826c1498834cdb0f856c67b4ad
Gmanka <gmankab@gmail.com>
2023-06-14 15:48:40 +0400

    fixed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 adress="bogdan@141.145.194.71"
 dir="car_parser"
 
+echo "adress = $adress"
+echo "dir = $dir"
+
 ssh -t "$adress" "rm -rf ~/$dir ; mkdir ~/$dir"
-scp delerships.txt main.py pyrightconfig.json setup.sh start.sh "$adress"":///home/bogdan/car_parser"
-ssh -t "$adress" "cd ~/$dir ; bash setup.sh"
+scp delerships.txt main.py pyrightconfig.json setup.sh start.sh "$adress"":///home/bogdan/$dir"
+ssh -t "$adress" "cd ~/$dir ; ls ; bash setup.sh"
 
